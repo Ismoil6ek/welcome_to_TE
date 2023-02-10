93fd048d029adffa13219ac58edb6f50d23c0fb6
Ismoil Ilxomov <ismoil6ek@gmail.com>
2023-02-10 21:23:35 +0500

    feat: task 3 finished

--- task_3/index.jsx
+++ task_3/index.jsx
@@ -1,18 +1,28 @@
-import { Fragment, memo } from 'react';
+import { Fragment, memo } from "react";
 
 const MainComponent = ({
-    user = { name: 'unknown', age: null } // default value for `props.user`
+  user = { name: "unknown", age: null }, // default value for `props.user`
 }) => {
-    return (
-        <Fragment>
-            <ChildComponent user={user} />
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <ChildComponent user={user} />
+    </Fragment>
+  );
 };
 
-// memoized component
+function areEqual(prevProps, nextProps) {
+  return (
+    prevProps.user.age === nextProps.user.age &&
+    prevProps.user.name === nextProps.user.name
+  );
+}
+
+// React memo takes callback as a second argument
+// using this, we can check if values are new, or the same
 const ChildComponent = memo(({ user: { name, age } }) => {
-    return (
-        <div>user name: {name}, user age: {age}</div>
-    )
-});
+  return (
+    <div>
+      user name: {name}, user age: {age}
+    </div>
+  );
+}, areEqual);
