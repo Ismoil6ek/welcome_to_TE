33e076da946c0ca396b2787c116d76ea15877a1e
Ismoil Ilxomov <ismoil6ek@gmail.com>
2023-02-10 21:02:45 +0500

    feat: task 2 finished

--- task_2/index.jsx
+++ task_2/index.jsx
@@ -1,16 +1,21 @@
-import { Fragment, memo } from 'react';
+import { Fragment, memo } from "react";
 
 const MainComponent = () => {
-    const makeLog = () => console.log('hi from MainComponent'); // function to make logs from MainComponent
+  // function makeLog causing child component rerender
+  // because it is different function at every state change
 
-    return (
-        <Fragment>
-            <ChildComponent makeLog={makeLog} />
-        </Fragment>
-    );
+  // so it is better to memorize function, it will be recreated
+  // if something changes in dependency array
+  const makeLog = useMemo(() => () => console.log("hi from MainComponent"), []); // function to make logs from MainComponent
+
+  return (
+    <Fragment>
+      <ChildComponent makeLog={makeLog} />
+    </Fragment>
+  );
 };
 
 // memoized component
 const ChildComponent = memo(({ makeLog }) => (
-    <button onClick={makeLog}>say Hi from ChildComponent</button>
+  <button onClick={makeLog}>say Hi from ChildComponent</button>
 ));
